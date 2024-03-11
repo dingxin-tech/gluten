869f95df84749a16c46a8cb6e3e4c7c436b029ee
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2024-03-11 14:26:58 +0800

    test: fix hive splitInfo test

--- cpp/velox/tests/Substrait2VeloxPlanConversionTest.cc
+++ cpp/velox/tests/Substrait2VeloxPlanConversionTest.cc
@@ -48,25 +48,25 @@ class Substrait2VeloxPlanConversionTest : public exec::test::HiveConnectorTestBa
     EXPECT_EQ(1, leafPlanNodeIds.size());
     const auto& splitInfo = splitInfos.at(*leafPlanNodeIds.begin());
 
-    const auto& paths = splitInfo->paths;
-    const auto& starts = splitInfo->starts;
-    const auto& lengths = splitInfo->lengths;
-    const auto fileFormat = splitInfo->format;
+//    const auto& paths = splitInfo->paths;
+//    const auto& starts = splitInfo->starts;
+//    const auto& lengths = splitInfo->lengths;
+//    const auto fileFormat = splitInfo->format;
 
     std::vector<std::shared_ptr<facebook::velox::connector::ConnectorSplit>> splits;
-    splits.reserve(paths.size());
-
-    for (int i = 0; i < paths.size(); i++) {
-      auto path = fmt::format("{}{}", tmpDir_->path, paths[i]);
-      auto start = starts[i];
-      auto length = lengths[i];
-      auto split = facebook::velox::exec::test::HiveConnectorSplitBuilder(path)
-                       .fileFormat(fileFormat)
-                       .start(start)
-                       .length(length)
-                       .build();
-      splits.emplace_back(split);
-    }
+//    splits.reserve(paths.size());
+//
+//    for (int i = 0; i < paths.size(); i++) {
+//      auto path = fmt::format("{}{}", tmpDir_->path, paths[i]);
+//      auto start = starts[i];
+//      auto length = lengths[i];
+//      auto split = facebook::velox::exec::test::HiveConnectorSplitBuilder(path)
+//                       .fileFormat(fileFormat)
+//                       .start(start)
+//                       .length(length)
+//                       .build();
+//      splits.emplace_back(split);
+//    }
     return splits;
   }
 
